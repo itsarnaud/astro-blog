82ea1018e79d9a5982d509bd9852696cc650ae66
Arnaud R. <arno.royer2004@gmail.com>
2025-10-17 16:56:14 +0200

    Feat: use svelte instead of react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from '@astrojs/preact';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-demo-1234.netlify.app',
-  integrations: [preact()]
+  integrations: [svelte()]
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,17 @@
+<script>
+  const { messages } = $props();
+
+  let greeting = $state(messages[0]);
+  
+  const random = () => {
+    return messages[(Math.floor(Math.random() * messages.length))];
+  }
+
+</script>
+
+<div>
+  <h3>{greeting}! Thank you for visiting!</h3>
+  <button onclick={() => greeting = random()}>
+    New Greeting
+  </button>
+</div>
